3efc0608b744eac623cc578b549d780ae864ad10
vidit-vats <46197417+vidit-vats@users.noreply.github.com>
2018-12-27 21:37:22 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+int calfac (int a);
+int main()
+{
+	int a,fact;
+	printf("Enter the number of which you want to find out the factorial\n");
+	scanf_s("%d", &a);
+	fact = calfac(a);
+	printf("Answer:%d\n", fact);
+
+	
+
+
+
+
+	return 0;
+}
+
+
+
+int calfac (int a)
+{
+	int i, f = 1;
+	for (i = 1; i <= a; i++)
+	{
+		f = f * i;
+	}
+	return f;
+}
